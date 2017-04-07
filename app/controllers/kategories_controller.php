7934b8d400f278ed004a4cad73bd3ba012141f47
Henriikka Karhuvara <henriikka.karhuvaara@helsinki.fi>
2017-04-07 11:24:34 +0300

    Uloskirjautuminen ja näkymien rajoitus

--- app/controllers/kategories_controller.php
+++ app/controllers/kategories_controller.php
@@ -3,11 +3,15 @@
 class KategoryController extends BaseController {
 
     public static function index() {
+        self::check_logged_in();
+
         $kategories = Kategory::all();
         View::make('kategories/index.html', array('kategories' => $kategories));
     }
 
     public static function show($id) {
+        self::check_logged_in();
+
         $kategory = Kategory::find($id);
         View::make('kategories/show.html', array('kategory' => $kategory));
     }
@@ -16,53 +20,54 @@ class KategoryController extends BaseController {
         $params = $_POST;
 
         $kategory = new Kategory(array(
-        'nimi' => $params['nimi'],
-        'lisays' => $params['lisays'],
-        'kuvaus' => $params['kuvaus']
-      ));
+            'nimi' => $params['nimi'],
+            'lisays' => $params['lisays'],
+            'kuvaus' => $params['kuvaus']
+        ));
 
         $kategory->save();
-        
+
         Redirect::to('/kategories/' . $kategory->id, array('message' => 'Kategoria on lisätty listaasi!'));
     }
-    
+
     public static function create() {
         //Näytetään askareen lisäyslomake
- 
+
         View::make('kategories/new.html');
     }
-    
+
     public static function edit($id) {
+        self::check_logged_in();
+
         $kategory = Kategory::find($id);
         View::make('kategories/edit.html', array('attributes' => $kategory));
     }
-    
+
     public static function update($id) {
         $params = $_POST;
-        
+
         $attributes = array(
             'id' => $id,
             'nimi' => $params['nimi'],
             'lisays' => $params['lisays'],
             'kuvaus' => $params['kuvaus']
         );
-        
+
         $kategory = new Kategory($attributes);
         $errors = $kategory->errors();
-        
-        if(count($errors) > 0) {
-            View::make('kategories/edit.html', array('errors'=>$errors,'attributes' => $attributes));
+
+        if (count($errors) > 0) {
+            View::make('kategories/edit.html', array('errors' => $errors, 'attributes' => $attributes));
         } else {
             $kategory->update();
-            Redirect::to('/kategories/'. $kategory->id,array('message' =>'Kategoriaa on muokattu onnistuneest'));
+            Redirect::to('/kategories/' . $kategory->id, array('message' => 'Kategoriaa on muokattu onnistuneest'));
         }
     }
-    
-    public static function destroy($id){
-    $kategory = new Kategory(array('id' => $id));
-    $kategory->destroy();
-    Redirect::to('/kategories/'. array('message' => 'Kategoria on poistettu onnistuneesti!'));
-  }
+
+    public static function destroy($id) {
+        $kategory = new Kategory(array('id' => $id));
+        $kategory->destroy();
+        Redirect::to('/kategories/' . array('message' => 'Kategoria on poistettu onnistuneesti!'));
+    }
 
 }
-
